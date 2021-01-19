c442f86eb0d3a78c7c2796ce237ddf2ec632bbd7
Aibek Soltobaev <kyubei.dark@gmail.com>
2021-01-19 15:20:24 +0600

    added class BookShop in models.py

--- main/models.py
+++ main/models.py
@@ -5,3 +5,13 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class BookShop(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.CharField(max_length=50)
+    genre = models.CharField(max_length=100)
+    author = models.CharField(max_length=150)
+    year = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
