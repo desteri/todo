076aed23df7dfd2d7ada83c1c073c560ea1dd03c
Aibek Soltobaev <kyubei.dark@gmail.com>
2021-01-19 22:21:35 +0600

    added todo form'

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo, BookShop
 
 def homepage(request):
@@ -26,3 +26,10 @@ def change(request):
 
 def delete(request):
     return render(request, "delete.html")
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second, third, add, change, delete, book
+from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path("add/", add),
     path("change/", change),
     path("delete/", delete),
-    path("book/", book)
+    path("book/", book),
+    path("add-todo/", add_todo, name="add-todo") 
 ]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
