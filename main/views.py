893effa54626dd8447ef048ed3695b77786125ab
Aibek Soltobaev <kyubei.dark@gmail.com>
2021-01-19 18:35:10 +0600

    added books.css and changes in books.html and views.py

--- main/views.py
+++ main/views.py
@@ -8,6 +8,10 @@ def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
+def book(request):
+    bookshop_list = BookShop.objects.all()
+    return render(request, "books.html", {"bookshop_list": bookshop_list})
+
 def second(request):
     return HttpResponse("test 2 page")
 
@@ -22,7 +26,3 @@ def change(request):
 
 def delete(request):
     return render(request, "delete.html")
-
-def book(request):
-    bookshop_list = BookShop.objects.all()
-    return render(request, "books.html", {"bookshop_list": bookshop_list})
