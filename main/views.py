e6c1e3cb3ef241ca2610a4b93820b0a907fc0a5c
Aibek Soltobaev <kyubei.dark@gmail.com>
2021-01-20 02:23:37 +0600

    added todo deletion

--- main/views.py
+++ main/views.py
@@ -47,3 +47,7 @@ def add_book(request):
     books.save()
     return redirect(book)
     
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
